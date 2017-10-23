fefe70d5a32d5f209ce91d9bdf2eb6aeda9a1763
jtonchuno <jtonchuno@gmail.com>
2017-10-23 09:40:34 -0500

    attempting read from database

--- js/map.js
+++ js/map.js
@@ -23,6 +23,8 @@ var map;
 var test1 = firebase.initializeApp(config);
 console.log(test1.name);
 
+var database = firebase.database();
+console.log(database.name);
 //attempt to put data from Firebase to console
 var androneRef = database.ref('User Data');
 androneRef.on('value', function(snapshot){
